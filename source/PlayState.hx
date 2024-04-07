10be61d7b290cf1e8299a6c11d54eae6e23ea2ec
akaz <82073147+akazdayo@users.noreply.github.com>
2024-04-08 01:28:41 +0900

    ✨ 音楽の追加

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -5,12 +5,14 @@ import KeyMap.Debug;
 import Notes.Note;
 import flixel.FlxG;
 import flixel.FlxState;
+import flixel.sound.FlxSound;
 import flixel.util.FlxTimer;
 
 class PlayState extends FlxState
 {
 	var notes:Array<Note>;
 	var judgeLine:JudgeLine;
+	var music:FlxSound;
 
 	override public function create()
 	{
@@ -24,7 +26,12 @@ class PlayState extends FlxState
 
 		// 判定クラスの初期化
 		judgeLine = new JudgeLine();
-		getChart('https://raw.githubusercontent.com/akazdayo/MusicArcade2/develop/assets/data/charts/test.csv', dropNotes);
+
+		// 音声の取得
+		music = FlxG.sound.load(AssetPaths.music__ogg);
+
+		// ゲーム開始
+		gameStart('https://raw.githubusercontent.com/akazdayo/MusicArcade2/develop/assets/data/charts/test.csv', dropNotes);
 	}
 
 	override public function update(elapsed:Float)
@@ -47,7 +54,7 @@ class PlayState extends FlxState
 	public function dropNotes(chart:Array<Array<Int>>)
 	{
 		var delay:Float = 0;
-
+		music.play();
 		for (x in chart)
 		{
 			new FlxTimer().start(delay, Void ->
@@ -68,7 +75,7 @@ class PlayState extends FlxState
 		}
 	}
 
-	public function getChart(url:String, callBack:Array<Array<Int>>->Void):Void
+	public function gameStart(url:String, callBack:Array<Array<Int>>->Void):Void
 	{
 		var chart = new Array<Array<Int>>();
 
